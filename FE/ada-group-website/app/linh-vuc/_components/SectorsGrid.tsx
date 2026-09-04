3b4b00deca9083d8601963fd36f7f95de0bb3422
nguyenhuutruong6666 <nguyenhuutruong6666@gmail.com>
2026-09-04 16:02:27 +0700

    refactor: update background color Hero linh-vuc and tin-tuc page

--- FE/ada-group-website/app/linh-vuc/_components/SectorsGrid.tsx
+++ FE/ada-group-website/app/linh-vuc/_components/SectorsGrid.tsx
@@ -7,7 +7,7 @@ export default function SectorsGrid() {
   const sectors = getSectors();
 
   return (
-    <section className="section-y">
+    <section className="section-y pt-0!">
       <div className="mx-auto max-w-360 px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
           {sectors.map((sector) => (
